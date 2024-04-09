5154a1e7732a9bbaaf51339cc238709f43054705
Sebastian <91025779+CodeMUGA@users.noreply.github.com>
2024-04-09 23:31:18 +0200

    form added with working submit button task

--- ih-todo-list/src/views/HomeView.vue
+++ ih-todo-list/src/views/HomeView.vue
@@ -1,11 +1,26 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const add_task = ref('')
+
+const addTask = () => {
+  if (add_task.value.trim() === '') {
+    return console.log('No hay tarea nueva')
+  } else {
+    return console.log('Tarea nueva:', add_task.value)
+  }
+}
+</script>
 
 <template>
   <div class="home">
     <h1>Home View!</h1>
+    <form @submit.prevent="addTask">
+      <h4>¿Que tienes por hacer?</h4>
+      <input type="text" placeholder="ej. Comprar leche" v-model="add_task" />
+      <input type="submit" value=" + " />
+    </form>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
